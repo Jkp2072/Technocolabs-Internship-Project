9c45b56997c9df153456858349bce3dd0baff1fc
Jkp2072 <64988456+Jkp2072@users.noreply.github.com>
2020-07-23 00:22:33 +0530

    Add files via upload

---
+++ projecttask7/main.js
@@ -0,0 +1,22 @@
+//age is assigned to myAge variable
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//new values is assigned to earlyYears variable
+console.log(`the earlyage is ${earlyYears} years`);
+
+//print the statement of earlyAge
+let laterYears = (myAge - 2) * 4;
+//using arithmetic operator and saving the value to laterYears variable
+console.log(`the laterage is ${laterYears} years`);
+
+//print the statement of laterAge
+let MyAgeInDogYears = laterYears + earlyYears;
+//addition laterYears and earlyYears id saved to MyAgeInDogYears
+let string = "jeet";
+myName = string.toLowerCase();
+//the toLowerCase is a method that returns the string to lowercase characters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${MyAgeInDogYears} years old in dog years.`);
+
+        
+//print the statement 
+    </script>
