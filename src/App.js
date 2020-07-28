e53367b2d37e33b80cd8bacd959b14f5dee50a0c
Jeet k Patel <64988456+Jkp2072@users.noreply.github.com>
2020-07-28 23:24:54 +0530

    Add files via upload
    only code files of react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const subject=props.subject;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello ,{subject}!
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
